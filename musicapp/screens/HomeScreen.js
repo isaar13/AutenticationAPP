64ddfa65e0182a586518c921ad8d1558dcb2477d
isaar13 <snaksrosario@gmail.com>
2025-03-17 21:14:58 -0100

    Descrição das mudanças

---
+++ musicapp/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+      <Button
+        title="Go to Todos"
+        onPress={() => navigation.navigate('Todo')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
