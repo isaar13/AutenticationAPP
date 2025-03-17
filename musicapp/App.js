01ff112d90a4a4bbd98b99731c623cfb21d58e9b
isaar13 <snaksrosario@gmail.com>
2025-03-17 21:03:41 -0100

    mudanças

--- musicapp/App.js
+++ musicapp/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    setTodos([...todos, text]);
+    setText('');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Add a todo"
+        value={text}
+        onChangeText={setText}
+      />
+      <Button title="Add Todo" onPress={addTodo} />
+      {todos.map((todo, index) => (
+        <Text key={index}>{todo}</Text>
+      ))}
     </View>
   );
 }
@@ -16,5 +35,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    width: '80%',
   },
 });
